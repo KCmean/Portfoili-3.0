0222f9a4ea4bc25a92e0349041203a94b1aaa538
Kunal Chaturvedi <96841396+KCmean@users.noreply.github.com>
2022-06-24 23:04:34 +0530

    Add files via upload

---
+++ java-script/index.js
@@ -0,0 +1,13 @@
+// for typing Animation
+
+var typed = new Typed('#typed', {
+   stringsElement: '#typed-strings',
+   typeSpeed : 20,
+   loop : true,
+   // startDelay : 500,
+   backSpeed : 20,
+   shuffle : true,
+   cursorChar: '<strong>|</strong>',
+}
+);
+
